9ea822de7ee3989e2a731f5a3e792ea6f46967d2
Minji <321963741@naver.com>
2021-11-05 09:47:01 +0900

    done 11/05

--- 21-cocktails/final/src/presentDrinks.js
+++ 21-cocktails/final/src/presentDrinks.js
@@ -1,12 +1,14 @@
 import fetchDrinks from './fetchDrinks.js';
 import displayDrinks from './displayDrinks.js';
 import setDrink from './setDrink.js';
+
 const showDrinks = async (url) => {
   // fetch drinks
   const data = await fetchDrinks(url);
-
+  console.log(data, 'data');
   // display drinks
   const section = await displayDrinks(data);
+  console.log(section, 'section');
   if (section) {
     setDrink(section);
   }
